1cacfd3e76a5e2705eff3a36495a4965cb190960
Ro'zmat <rozmatjonotajonov@gmail.com>
2024-06-07 01:12:21 +0500

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+## Shortcuts for rapid development
---
+++ lib/cat.dart
@@ -0,0 +1,25 @@
+library cat;
+
+// export tools/utilities
+export 'package:cat/src/tools/shortcuts.dart';
+export 'package:cat/src/tools/snackbar.dart';
+export 'package:cat/src/tools/check_internet.dart';
+export 'package:cat/src/tools/vibration.dart';
+export 'package:cat/src/tools/url_launcher.dart';
+export 'package:cat/src/tools/share_plus.dart';
+export 'package:cat/src/tools/loading_dialog.dart';
+export 'package:cat/src/tools/log.dart';
+
+
+// export widgets
+export 'package:cat/src/widgets/text_input.dart';
+export 'package:cat/src/widgets/container_card.dart';
+export 'package:cat/src/widgets/bubble_bottom_bar.dart';
+export 'package:cat/src/widgets/profile_picture.dart';
+
+export 'package:loading_animation_widget/loading_animation_widget.dart';
+export 'package:get/get.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:flutter/material.dart';
+
+export 'package:lottie/lottie.dart';
