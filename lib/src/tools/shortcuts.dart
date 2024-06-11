97c802e4dee2d8f02c017d76e3608c0d71f5f898
Ro'zmat <rozmatjonotajonov@gmail.com>
2024-06-11 23:54:22 +0500

    Page route improvements

--- lib/src/tools/shortcuts.dart
+++ lib/src/tools/shortcuts.dart
@@ -1,21 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-bool routePage( Widget page, {bool canOnlySwipeFromEdge = false} ) {
+enum RouteStyle {material, cupertino}
+
+bool routePage( Widget page, {BuildContext? context, bool canOnlySwipeFromEdge = false, RouteStyle routeStyle = RouteStyle.material} ) {
 
   // Navigator.of(Get.context!).push(SwipeablePageRoute(
   //   canOnlySwipeFromEdge: canOnlySwipeFromEdge,
   //   builder: (BuildContext context) => page,
   // ));
 
+
+  if(routeStyle == RouteStyle.material){
+    Navigator.push(
+      context ?? Get.context!,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  } else if(routeStyle == RouteStyle.cupertino) {
+    Navigator.push(
+      context ?? Get.context!,
+      CupertinoPageRoute(builder: (context) => page),
+    );
+  }
+
   return true;
 }
 
-bool jumpPage( Widget page ) {
 
-    Navigator.pushReplacement( Get.context!,
+bool jumpPage( Widget page, {BuildContext? context, RouteStyle routeStyle = RouteStyle.material} ) {
+
+
+  if(routeStyle == RouteStyle.material){
+    Navigator.pushReplacement( context ?? Get.context!,
       MaterialPageRoute(builder: (context) => page ),
     );
+  } else {
+    Navigator.pushReplacement( context ?? Get.context!,
+      CupertinoPageRoute(builder: (context) => page ),
+    );
+  }
     
   return true;
 }
